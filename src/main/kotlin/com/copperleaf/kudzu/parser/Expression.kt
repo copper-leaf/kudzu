2f1ebc2e5bccb87543db16f3a11fb60e15929266
Casey Brooks <cjbrooks12@gmail.com>
2018-10-09 14:11:10 -0500

    Implements operator table parser

--- src/main/kotlin/com/copperleaf/kudzu/parser/Expression.kt
+++ src/main/kotlin/com/copperleaf/kudzu/parser/Expression.kt
@@ -4,15 +4,15 @@ import com.copperleaf.kudzu.Node
 import com.copperleaf.kudzu.Parser
 import com.copperleaf.kudzu.ParserContext
 
-class ExpressionParser(name: String = "") : Parser(name) {
+class ExpressionParser(termParser: Parser, name: String = "") : Parser(name) {
 
-    val parser: Parser = setupExpressionParserWithFixedLevels()
+    val parser: Parser = setupExpressionParserWithFixedLevels(termParser)
 
     override fun predict(input: ParserContext): Boolean = parser.predict(input)
 
     override fun parse(input: ParserContext): Pair<Node, ParserContext> = parser.parse(input)
 
-    private fun setupExpressionParserWithFixedLevels(): Parser {
+    private fun setupExpressionParserWithFixedLevels(termParser: Parser): Parser {
         val EParser = LazyParser(name = "EParser")
         val E = LazyParser(name = "E")
         val T = LazyParser(name = "T")
@@ -62,7 +62,7 @@ class ExpressionParser(name: String = "") : Parser(name) {
         F.parser = _F
 
         val _P = ChoiceParser(
-                CharParser(name = "val"),
+                termParser,
                 SequenceParser(
                         CharInParser('('),
                         E,
@@ -84,3 +84,84 @@ class ExpressionParser(name: String = "") : Parser(name) {
     }
 
 }
+
+
+// Adapted from jparsec OperatorTable class
+//----------------------------------------------------------------------------------------------------------------------
+
+class OperatorTable(
+        private val operand: Parser,
+        private vararg val operators: Operator,
+        name: String = ""
+) : Parser(name) {
+
+    private val expressionParser = buildExpressionParser()
+
+    private fun buildExpressionParser(): Parser {
+        if (operators.isEmpty()) return operand
+
+        var startIndex = 0
+        var endIndex = 0
+        var precedence = operators.first().precedence
+        var associativity = operators.first().associativity
+        var expressionParser = operand
+        for (i in 1 until operators.size) {
+            val op = operators[i]
+            endIndex = i
+            if (op.precedence == precedence && op.associativity == associativity) {
+                continue
+            }
+            endIndex = i
+            expressionParser = buildSubExpression(getOperatorParser(startIndex, endIndex), associativity, expressionParser)
+            startIndex = i
+            precedence = operators[i].precedence
+            associativity = operators[i].associativity
+        }
+        if (endIndex != operators.size) {
+            endIndex = operators.size
+            associativity = operators[startIndex].associativity
+            expressionParser = buildSubExpression(getOperatorParser(startIndex, endIndex), associativity, expressionParser)
+        }
+        return expressionParser
+    }
+
+    private fun getOperatorParser(begin: Int, end: Int): Parser {
+        return ChoiceParser(*operators.slice(begin until end).map { it.op }.toTypedArray(), name="op")
+    }
+
+    private fun buildSubExpression(operator: Parser, associativity: Associativity, operand: Parser): Parser {
+        return when (associativity) {
+            Associativity.PREFIX  -> SequenceParser(ManyParser(operator), operand, name="prefix")
+            Associativity.POSTFIX -> SequenceParser(operand, ManyParser(operator), name="postfix")
+            Associativity.INFIX   -> SequenceParser(operand, ManyParser(SequenceParser(operator, operand)), name="infix")
+        }
+    }
+
+// Parse with this expression
+//----------------------------------------------------------------------------------------------------------------------
+
+    override fun predict(input: ParserContext): Boolean = expressionParser.predict(input)
+
+    override fun parse(input: ParserContext): Pair<Node, ParserContext> = expressionParser.parse(input)
+}
+
+private enum class Associativity {
+    PREFIX, POSTFIX, INFIX
+}
+
+sealed class Operator(
+        val op: Parser,
+        val precedence: Int,
+        val associativity: Associativity
+) : Comparable<Operator> {
+
+    /** Higher precedence first. For tie, compares associativity.  */
+    override fun compareTo(other: Operator): Int {
+        if (precedence > other.precedence) return -1
+        return if (precedence < other.precedence) 1 else associativity.compareTo(other.associativity)
+    }
+}
+
+class PrefixOperator(op: Parser, precedence: Int) : Operator(op, precedence, Associativity.PREFIX)
+class PostfixOperator(op: Parser, precedence: Int) : Operator(op, precedence, Associativity.POSTFIX)
+class InfixOperator(op: Parser, precedence: Int) : Operator(op, precedence, Associativity.INFIX)
--- src/test/kotlin/com/copperleaf/kudzu/parser/TestExpression.kt
+++ src/test/kotlin/com/copperleaf/kudzu/parser/TestExpression.kt
@@ -10,10 +10,59 @@ import strikt.api.expectThat
 class TestExpression {
 
     @Test
-    fun testWordParser() {
+    fun testFixedExpressionParser() {
         var input: String
         var output: Pair<Node, ParserContext>?
-        val underTest = ExpressionParser()
+        val termParser = AtLeastParser(1, DigitParser(name = "val"))
+        val underTest = ExpressionParser(termParser)
+
+        input = "11 ^ 22 * 33 + 44 + 55"
+        output = underTest.test(input, skipWhitespace = true)
+
+        expectThat(output)
+                .parsedCorrectly()
+                .thenLog()
+    }
+
+    @Test
+    fun testOperatorTableExpressionParser() {
+        var input: String
+        var output: Pair<Node, ParserContext>?
+
+        val operatorTable = LazyParser()
+        val valueParser = LazyParser()
+        val termParser = LazyParser()
+
+        val _operatorTable = OperatorTable(
+                termParser,
+                InfixOperator(WordParser("||"), 10),
+                InfixOperator(WordParser("&&"), 20),
+                InfixOperator(WordParser("=="), 30),
+                InfixOperator(CharInParser('+'), 40),
+                InfixOperator(CharInParser('-'), 40),
+                PrefixOperator(CharInParser('-'), 50),
+                InfixOperator(CharInParser('*'), 60),
+                InfixOperator(CharInParser('/'), 60),
+                InfixOperator(CharInParser('^'), 70)
+        )
+        operatorTable.parser = _operatorTable
+
+        val _valueParser = CharParser(name = "val")
+        valueParser.parser = _valueParser
+
+        val _termParser = ChoiceParser(
+                SequenceParser(
+                        CharInParser('('),
+                        operatorTable,
+                        CharInParser(')'),
+                        name = "parens"
+                ),
+                valueParser,
+                name = "term"
+        )
+        termParser.parser = _termParser
+
+        val underTest = operatorTable
 
         input = "a ^ b * c + d + e"
         output = underTest.test(input, skipWhitespace = true)
