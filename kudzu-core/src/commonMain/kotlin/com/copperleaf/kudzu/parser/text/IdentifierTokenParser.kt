a9b92ac57d9591f17b8bf3a6cc95436f55d2a6cf
Casey Brooks <cjbrooks12@gmail.com>
2021-04-23 12:09:43 -0500

    Adds generic identifier parser

---
+++ kudzu-core/src/commonMain/kotlin/com/copperleaf/kudzu/parser/text/IdentifierTokenParser.kt
@@ -0,0 +1,46 @@
+package com.copperleaf.kudzu.parser.text
+
+import com.copperleaf.kudzu.node.text.TextNode
+import com.copperleaf.kudzu.parser.Parser
+import com.copperleaf.kudzu.parser.ParserContext
+import com.copperleaf.kudzu.parser.ParserResult
+import com.copperleaf.kudzu.parser.chars.CharInParser
+import com.copperleaf.kudzu.parser.chars.LetterOrDigitParser
+import com.copperleaf.kudzu.parser.chars.LetterParser
+import com.copperleaf.kudzu.parser.choice.ExactChoiceParser
+import com.copperleaf.kudzu.parser.many.ManyParser
+import com.copperleaf.kudzu.parser.mapped.FlatMappedParser
+import com.copperleaf.kudzu.parser.maybe.MaybeParser
+import com.copperleaf.kudzu.parser.sequence.SequenceParser
+
+/**
+ * Consume a token that is a valid, generic identifier. An identifier is a letter or underscore followed by zero or more letter,
+ * digit, or underscore characters.
+ */
+@ExperimentalStdlibApi
+class IdentifierTokenParser : Parser<TextNode> {
+    private val parser: Parser<TextNode> by lazy {
+        FlatMappedParser(
+            SequenceParser(
+                ExactChoiceParser(
+                    LetterParser(),
+                    CharInParser('_'),
+                ),
+                MaybeParser(
+                    ManyParser(
+                        ExactChoiceParser(
+                            LetterOrDigitParser(),
+                            CharInParser('_'),
+                        )
+                    )
+                ),
+            )
+        ) {
+            TextNode(it.text, it.context)
+        }
+    }
+
+    override fun predict(input: ParserContext): Boolean = parser.predict(input)
+
+    override val parse: DeepRecursiveFunction<ParserContext, ParserResult<TextNode>> = parser.parse
+}
---
+++ kudzu-core/src/commonTest/kotlin/com/copperleaf/kudzu/parser/text/TestIdentifier.kt
@@ -0,0 +1,53 @@
+package com.copperleaf.kudzu.parser.text
+
+import com.copperleaf.kudzu.expectThat
+import com.copperleaf.kudzu.isEqualTo
+import com.copperleaf.kudzu.isNotNull
+import com.copperleaf.kudzu.node
+import com.copperleaf.kudzu.parsedCorrectly
+import com.copperleaf.kudzu.parsedIncorrectly
+import com.copperleaf.kudzu.test
+import kotlin.test.Test
+
+@OptIn(ExperimentalStdlibApi::class)
+class TestIdentifier {
+
+    @Test
+    fun testIdentifierTokenParser() {
+        val parser = IdentifierTokenParser()
+
+        val inputs = listOf(
+            "a" to "a",
+            "ab" to "ab",
+            "a1" to "a1",
+            "a_" to "a_",
+
+            "1" to null,
+            "1a" to null,
+            "11" to null,
+            "1_" to null,
+
+            "_" to "_",
+            "_a" to "_a",
+            "_1" to "_1",
+            "__" to "__",
+
+            "_a1_b2_c3_" to "_a1_b2_c3_",
+        )
+
+        inputs.map { (input, expectedResult) ->
+            val output = parser.test(input)
+
+            if (expectedResult != null) {
+                expectThat(output)
+                    .parsedCorrectly()
+                    .node()
+                    .isNotNull()
+                    .also { it.text.isEqualTo(expectedResult) }
+            } else {
+                expectThat(output)
+                    .parsedIncorrectly()
+            }
+        }
+    }
+}
