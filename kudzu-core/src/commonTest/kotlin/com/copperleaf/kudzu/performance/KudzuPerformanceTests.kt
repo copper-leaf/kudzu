5e4431fb7c8dd1301370d1a9e34355a55a44420c
Casey Brooks <cjbrooks12@gmail.com>
2021-08-17 14:54:51 -0500

    disable performance tests

--- kudzu-core/src/commonTest/kotlin/com/copperleaf/kudzu/performance/KudzuPerformanceTests.kt
+++ kudzu-core/src/commonTest/kotlin/com/copperleaf/kudzu/performance/KudzuPerformanceTests.kt
@@ -12,7 +12,7 @@ import kotlin.test.Test
 @OptIn(ExperimentalStdlibApi::class)
 class KudzuPerformanceTests {
 
-    val enabled = true
+    val enabled = false
 
     val simpleExpression = run {
         "1 - 2 * (3 + 4 / 5 ^ 6 * (7 - 8)) * 9"
