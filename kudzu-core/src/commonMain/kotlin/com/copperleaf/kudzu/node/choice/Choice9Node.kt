5439d955cb53c7f4c192a4ea15dac77d5a6c1d88
Daniel Lin <ephemient@gmail.com>
2022-10-02 01:09:06 -0400

    Make it possible to process choice nodes with type safety
    Instead of requiring casting to access
    
    ```kotlin
    MappedParser(ChoiceParser(FooParser(), BarParser())) { node ->
        when (node) {
            is Choice2Node.Option1 -> (node.node as FooNode).foo
            is Choice2Node.Option2 -> (node.node as BarNode).bar
        }
    }
    ```
    
    we can rely on the compiler to check
    
    ```kotlin
    MappedParser(ChoiceParser(FooParser(), BarParser())) { node ->
        when (node) {
            is Choice2Node.Option1 -> node.node.foo
            is Choice2Node.Option2 -> node.node.bar
        }
    }
    ```

--- kudzu-core/src/commonMain/kotlin/com/copperleaf/kudzu/node/choice/Choice9Node.kt
+++ kudzu-core/src/commonMain/kotlin/com/copperleaf/kudzu/node/choice/Choice9Node.kt
@@ -6,46 +6,47 @@ import com.copperleaf.kudzu.node.NonTerminalNode
 
 /* ktlint-disable max-line-length */
 sealed class Choice9Node<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-    val node: Node,
+    node: Node,
     context: NodeContext
 ) : NonTerminalNode(context) {
+    abstract val node: Node
     override val children: List<Node> = listOf(node)
     override val astNodeName: String get() = "Choice9Node.${this::class.simpleName!!}"
 
     class Option1<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T1,
+        override val node: T1,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option2<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T2,
+        override val node: T2,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option3<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T3,
+        override val node: T3,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option4<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T4,
+        override val node: T4,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option5<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T5,
+        override val node: T5,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option6<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T6,
+        override val node: T6,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option7<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T7,
+        override val node: T7,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option8<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T8,
+        override val node: T8,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
     class Option9<T1 : Node, T2 : Node, T3 : Node, T4 : Node, T5 : Node, T6 : Node, T7 : Node, T8 : Node, T9 : Node>(
-        node: T9,
+        override val node: T9,
         context: NodeContext
     ) : Choice9Node<T1, T2, T3, T4, T5, T6, T7, T8, T9>(node, context)
 }
